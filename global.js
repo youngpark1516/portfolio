ebd9a1727adbe2b0ec15e290a68e4c286bba9170
Chanyoung Park <cypark1516@gmail.com>
2025-01-24 22:02:28 -0800

    lab3

---
+++ global.js
@@ -0,0 +1,75 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+// const navLinks = $$("nav a");
+// const currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname
+// );
+// currentLink?.classList.add("current");
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'CV.html', title: 'CV' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'https://github.com/youngpark1516', title: 'GitHub' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+
+    a.classList.toggle(
+        'current',
+        a.host === location.host && a.pathname === location.pathname
+    );
+
+    if (a.host !== location.host) {
+        a.target = "_blank";
+    }
+    
+    a.toggleAttribute('target', a.host !== location.host);
+    nav.append(a);
+}
+
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <label class="color-scheme">
+      Theme:
+      <select>
+        <option value="light dark">Automatic</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </label>
+    `
+);
+
+const select = document.querySelector('.color-scheme select');
+
+function setColorScheme(colorScheme) {
+    document.documentElement.style.setProperty('color-scheme', colorScheme);
+    select.value = colorScheme;
+}
+
+const savedScheme = localStorage.colorScheme || 'light dark';
+setColorScheme(savedScheme);
+
+select.addEventListener('input', (event) => {
+    const newScheme = event.target.value;
+    setColorScheme(newScheme);
+    localStorage.colorScheme = newScheme;
+});
+
+  
