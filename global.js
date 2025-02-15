f3774907881d481f6204d955f48898edefc2b92d
Chanyoung Park <cypark1516@gmail.com>
2025-02-14 23:19:39 -0800

    lab6-3

--- global.js
+++ global.js
@@ -114,3 +114,4 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
 export async function fetchGitHubData(username) {
     return fetchJSON(`https://api.github.com/users/${username}`);
 }
+
